33c94863919fbc8d96d60e44f59c3ef6bc7824fd
euklyd <euklyd@users.noreply.github.com>
2019-06-17 22:43:12 -0700

    update schema to reflect modern sqlalchemy practices, add reprs

--- plugins/hostbot_schema.py
+++ plugins/hostbot_schema.py
@@ -1,6 +1,5 @@
 from sqlalchemy import ForeignKey
 from sqlalchemy import Column, Integer, String
-
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -11,19 +10,40 @@ Base = declarative_base()
 class Server(Base):
     __tablename__ = 'Server'
     id       = Column(Integer, primary_key=True)
-    roles    = relationship('Role', backref='A.id')
-    channels = relationship('Channel', backref='A.id')
+    name     = Column(String)
+    sheet    = Column(String)
+    roles    = relationship('Role', back_populates='server')
+    channels = relationship('Channel', back_populates='server')
+
+    def __repr__(self):
+        return f'<Server id={self.id}, name={self.name}, sheet={self.sheet}>'
 
 
 class Role(Base):
     __tablename__ = 'Role'
     id        = Column(Integer, primary_key=True)
     type      = Column(String)
-    server_id = Column(Integer, ForeignKey='Server.id')
+    server_id = Column(Integer, ForeignKey('Server.id'))
+    server    = relationship('Server', back_populates='roles')
+
+    def __repr__(self):
+        return (
+            f'<Role id={self.id}, '
+            f'type={self.type}, '
+            f'server_id={self.server_id}>'
+        )
 
 
 class Channel(Base):
     __tablename__ = 'Channel'
     id        = Column(Integer, primary_key=True)
     type      = Column(String)
-    server_id = Column(Integer, ForeignKey='Server.id')
+    server_id = Column(Integer, ForeignKey('Server.id'))
+    server    = relationship('Server', back_populates='channels')
+
+    def __repr__(self):
+        return (
+            f'<Channel id={self.id}, '
+            f'type={self.type}, '
+            f'server_id={self.server_id}>'
+        )
