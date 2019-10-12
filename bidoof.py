fcd8c4307a36741d5dd87e40f4360d734aa6c366
euklyd <euklyd@users.noreply.github.com>
2019-10-12 03:04:34 -0700

    turns out i'm supposed to update this too

--- bidoof.py
+++ bidoof.py
@@ -3,18 +3,24 @@ from discord.ext import commands
 
 import conf.settings as settings
 
-bot = commands.Bot(
+from core.bot import Bot
+
+bot = Bot(
     command_prefix=settings.prefix,
-    description='https://board8.fandom.com/wiki/Mafia_Bidoof'
+    description='https://board8.fandom.com/wiki/Mafia_Bidoof',
+    conf=settings.conf,
+    owner_id=settings.owner_id,
 )
 
 for plugin in settings.plugins:
     bot.load_extension(f'plugins.{plugin}')
 
+
 @bot.command()
 async def reload(ctx, plugin):
     bot.reload_extension(f'plugins.{plugin}')
     await ctx.send(f'Reloaded plugin `{plugin}`.')
+
 
 @bot.command()
 async def shutdown(ctx):
