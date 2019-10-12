41c89ee4fddc8b31573745421b108600ad6590c5
euklyd <euklyd@users.noreply.github.com>
2019-10-12 03:06:22 -0700

    add color preview

---
+++ plugins/fun.py
@@ -0,0 +1,26 @@
+import io
+
+from PIL import Image
+
+import discord
+from discord.ext import commands
+
+
+@commands.command()
+async def color(ctx: commands.context.Context, hexcode):
+    if hexcode[0] == '#':
+        color = hexcode
+    else:
+        color = f'#{hex(int(hexcode, 16))[2:]}'
+    # color = ((hexcode >> 16) & 0xff, (hexcode >> 8) & 0xff, hexcode & 0xff)
+    im = Image.new('RGB', size=(128, 128), color=color)
+    img_bytes = io.BytesIO()
+    filename = f'{color}.png'
+    im.save(img_bytes, format='PNG')
+    img_bytes.seek(0)
+    f = discord.File(img_bytes, filename=filename)
+    await ctx.send(file=f)
+
+
+def setup(bot):
+    bot.add_command(color)
