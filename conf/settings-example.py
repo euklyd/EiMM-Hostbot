0d41fb81d8fff2bb948f2fe7a73489adad565d59
euklyd <euklyd@users.noreply.github.com>
2019-10-12 02:56:17 -0700

    add example settings

---
+++ conf/settings-example.py
@@ -0,0 +1,18 @@
+from conf.conf import Conf
+
+
+name         = 'mafia bidoof'
+owner_id     = 123456789012345678
+client_token = 'your token here'
+
+plugins = [
+    'hostbot'
+]
+
+prefix = ['##']
+
+conf = Conf(
+    greentick_id=632481525325103114,
+    redtick_id=632481525979676712,
+    plugins=plugins,
+)
