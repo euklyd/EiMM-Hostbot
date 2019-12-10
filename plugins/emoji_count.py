32f38816de0c47e6ece131903973b8fff085d133
euklyd <euklyd@users.noreply.github.com>
2019-12-10 00:52:46 -0800

    add emoji frequency counting v1.0

---
+++ plugins/emoji_count.py
@@ -0,0 +1,144 @@
+import json
+import re
+from pathlib import Path
+from typing import Union, Callable, Optional, List
+from datetime import datetime, timedelta
+
+import discord
+from discord.ext import commands
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker, Session
+
+import plugins.emoji_schema as es
+
+_EMOJI_RE = re.compile(r'<:(?P<name>\w\w+):(?P<id>\d+)>')
+
+session_maker = None  # type: Union[None, Callable[[], Session]]
+enabled_servers = []  # type: List[int]  # discord server IDs
+enabled_servers_path = 'databases/conf/emoji_enabled_servers.json'
+
+
+def increment_count(session: Session, server: discord.guild, emoji_id: int, today: datetime.date) -> int:
+    entry = session.query(es.EmojiCount).filter_by(
+        server_id=server.id, emoji_id=emoji_id, date=today).one_or_none()  # type: Optional[es.EmojiCount]
+
+    if entry is not None:
+        entry.count += 1
+    else:
+        # make a new entry
+        entry = es.EmojiCount(server_id=server.id, emoji_id=emoji_id, date=today, count=1)
+        session.add(entry)
+    session.commit()
+    return entry.count
+
+
+async def count_emoji(message: discord.Message):
+    if message.author.bot:
+        # nope
+        return
+    session = session_maker()
+
+    emoji_ids = {e.id: e for e in message.guild.emojis}
+
+    today = datetime.utcnow().date()
+
+    found = []
+
+    for match in _EMOJI_RE.finditer(message.content):
+        emoji_id = int(match.group('id'))  # type: int  # TODO
+        if emoji_id in emoji_ids:
+            # only increment the count if it's an actual emoji that belongs to the server
+            count = increment_count(session, message.guild, emoji_id, today)
+            await message.channel.send(f'Count for {emoji_ids[emoji_id]} is now {count}.')
+
+
+@commands.group(invoke_without_command=False)
+async def emoji(ctx: commands.context.Context):
+    await ctx.send('nah')
+
+
+@emoji.command(name='enable')
+async def emoji_enable(ctx: commands.context.Context):
+    """
+    Enable emoji counting on the current server.
+    """
+    global enabled_servers
+    if ctx.guild.id not in enabled_servers:
+        enabled_servers.append(ctx.guild.id)
+        with open(enabled_servers_path, 'w') as enabled:
+            json.dump(enabled_servers, enabled, indent=4)
+        await ctx.send(f'Emoji counting enabled on **{ctx.guild}**.')
+    else:
+        await ctx.send(f'Emoji counting already enabled; use `disable` to turn it off.')
+
+
+@emoji.command(name='disable')
+async def emoji_disable(ctx: commands.context.Context):
+    """
+    Disable emoji counting on the current server.
+    """
+    global enabled_servers
+    if ctx.guild.id in enabled_servers:
+        enabled_servers.remove(ctx.guild.id)
+        with open(enabled_servers_path, 'w') as enabled:
+            json.dump(enabled_servers, enabled, indent=4)
+        await ctx.send(f'Emoji counting disabled on **{ctx.guild}**.')
+    else:
+        await ctx.send(f'Emoji counting not enabled; use `enable` to turn it on.')
+
+
+@emoji.command(name='count')
+async def emoji_count(ctx: commands.context.Context, em: Union[discord.Emoji, int], days: Optional[int] = 30):
+    """
+    Count the times a given emoji has been used on this server in the last <days> days.
+    """
+    if type(em) is discord.Emoji:
+        assert type(em) is discord.Emoji
+        emoji_id = em.id
+    else:
+        emoji_id = em
+
+    oldest = datetime.utcnow().date() - timedelta(days=days)
+
+    session = session_maker()
+    entries = session.query(es.EmojiCount).filter_by(
+        server_id=ctx.guild.id, emoji_id=emoji_id).filter(
+        func.DATE(es.EmojiCount.date) > oldest)  # type: List[es.EmojiCount]
+
+    count = 0
+    for entry in entries:
+        count += entry.count
+
+    if count == 1:
+        n_times = f'{count} time'
+    else:
+        n_times = f'{count} times'
+    if days == 1:
+        n_days = f'{days} day'
+    else:
+        n_days = f'{days} days'
+    await ctx.send(f'{ctx.bot.get_emoji(emoji_id)} has been used `{n_times}` in the last `{n_days}`.')
+
+
+def setup(bot: commands.Bot):
+    global enabled_servers
+    global session_maker
+
+    db_dir = 'databases/'
+    db_file = f'{db_dir}/emojicount.db'
+    if not Path(db_file).exists():
+        Path(db_dir).mkdir(exist_ok=True)
+
+    engine = create_engine(f'sqlite:///{db_file}')
+    session_maker = sessionmaker(bind=engine)
+
+    es.Base.metadata.create_all(engine)
+
+    if Path(enabled_servers_path).exists():
+        with open(enabled_servers_path, 'r') as enabled:
+            enabled_servers = json.load(enabled)
+    else:
+        enabled_servers = []
+
+    bot.add_listener(count_emoji, 'on_message')
+    bot.add_command(emoji)
---
+++ plugins/emoji_schema.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Date, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class EmojiCount(Base):
+    __tablename__ = 'EmojiCount'
+    emoji_id = Column(Integer, primary_key=True)
+    server_id = Column(Integer, primary_key=True)
+    date = Column(Date, primary_key=True)
+    count = Column(Integer)
+
+    def __repr__(self):
+        return \
+            f'<EmojiCount emoji_id={self.emoji_id}, server_id={self.server_id}, date={self.date}>, count={self.count}'
