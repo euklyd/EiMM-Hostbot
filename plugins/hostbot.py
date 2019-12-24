08f6232042625c5afa28f0070fbd2e1f633099f6
euklyd <euklyd@users.noreply.github.com>
2019-12-24 06:09:57 -0800

    add role pm creation + fix a bunch of channel overrides

--- plugins/hostbot.py
+++ plugins/hostbot.py
@@ -1,15 +1,14 @@
+import pprint
+import re
+from pathlib import Path
 from typing import Callable, Union
 
 import discord
-from discord.ext import commands
-
-import pprint
 import yaml
-
-from pathlib import Path
-
+from discord.ext import commands
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
+
 import plugins.hostbot_schema as hbs
 
 session_maker = None  # type: Union[None, Callable[[], Session]]
@@ -62,22 +61,32 @@ async def init_server(ctx: commands.Context, *, yml):
 
     for key, channel_name in yml['channels'].items():
         roles = yml['roles']
-        overwrites = {}
+        overwrites = {
+            ctx.guild.default_role: discord.PermissionOverwrite(send_messages=False),
+            roles['host']['role']: discord.PermissionOverwrite(
+                read_messages=True, send_messages=True, manage_messages=True),
+        }
         if key == 'gamechat':
-            overwrites[roles['player']['role']]: discord.PermissionOverwrite(send_messages=True)
-        else:
-            overwrites[roles['player']['role']]: discord.PermissionOverwrite(send_messages=False)
+            overwrites[roles['player']['role']] = discord.PermissionOverwrite(send_messages=True)
+            print(f'gamechat: creating {channel_name} for {key}')
         if key == 'graveyard':
-            overwrites[roles['player']['role']]: discord.PermissionOverwrite(read_messages=False)
-            overwrites[roles['dead']['role']]: discord.PermissionOverwrite(send_messages=True)
-        else:
-            overwrites[roles['dead']['role']]: discord.PermissionOverwrite(send_messages=False)
+            overwrites[ctx.guild.default_role] = discord.PermissionOverwrite(read_messages=False, send_messages=None)
+            # overwrites[roles['player']['role']] = discord.PermissionOverwrite(read_messages=False)
+            overwrites[roles['dead']['role']] = discord.PermissionOverwrite(read_messages=True)
+            overwrites[roles['host']['role']] = discord.PermissionOverwrite(read_messages=True)
+            overwrites[roles['spec']['role']] = discord.PermissionOverwrite(read_messages=True)
+            print(f'graveyard: creating {channel_name} for {key}')
+
+        if key == 'music':
+            overwrites[ctx.guild.default_role] = discord.PermissionOverwrite(read_messages=False)
 
         new_channel = await ctx.guild.create_text_channel(
             name=channel_name,
-            overwrites=overwrites
+            overwrites=overwrites,
         )
         channels.append(hbs.Channel(id=new_channel.id, type=key))
+        # pprint.pprint(f'created {channel_name} `[{key}]` with overwrites:\n{overwrites}\n')
+        print(f'created {channel_name} `[{key}]` with overwrites:\n{pprint.pformat(overwrites)}\n')
     server.channels = channels
 
     session.add(server)
@@ -86,8 +95,80 @@ async def init_server(ctx: commands.Context, *, yml):
     await ctx.send('created a bunch of things')
 
 
-@init.command(name='reset')
+def player_channel_name(player: discord.Member):
+    name = player.name
+    name = re.sub(r'[\W_ -]+', '', name)
+    name = re.sub(r' ', '-', name)
+    return f'{name}_{player.discriminator}'
+
+
+@init.command(name='rolepms')
 @commands.has_permissions(administrator=True)
+async def init_rolepms(ctx: commands.Context):
+    # ls_usernames = get_from_sheet()  # TODO
+
+    session = session_maker()
+    spec_role_id = session.query(hbs.Role).filter_by(server_id=ctx.guild.id, type='spec').one_or_none()
+    spec_role = ctx.guild.get_role(spec_role_id.id)
+
+    host_role_id = session.query(hbs.Role).filter_by(server_id=ctx.guild.id, type='host').one_or_none()
+    host_role = ctx.guild.get_role(host_role_id.id)
+
+    player_role_id = session.query(hbs.Role).filter_by(server_id=ctx.guild.id, type='player').one_or_none()
+    player_role = ctx.guild.get_role(player_role_id.id)
+
+    print(f'spec_role_id: {spec_role_id}')
+    print(f'host_role_id: {spec_role_id}')
+
+    ls_usernames = [
+        'BT#4881',
+        'Kim#9000',
+        'Monde#6197',
+    ]
+
+    players = []
+    error_names = []
+    error = 'Error finding players: ```\n'
+    for name in ls_usernames:
+        player = ctx.guild.get_member_named(name)
+        if player is None:
+            error_names.append(name)
+            error += f'{name}\n'
+        else:
+            players.append(player)
+    error += '```'
+
+    players = sorted(players, key=lambda p: p.name.lower())
+    category = await ctx.guild.create_category('Role PMs', overwrites={
+        ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        ctx.guild.me: discord.PermissionOverwrite(read_messages=True),
+        host_role: discord.PermissionOverwrite(read_messages=True, manage_messages=True),
+        spec_role: discord.PermissionOverwrite(read_messages=True),
+    })  # type: discord.CategoryChannel
+    for player in players:
+        await player.edit(roles=player.roles + [player_role])
+        overwrites = {
+            ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            ctx.guild.me: discord.PermissionOverwrite(read_messages=True),
+            player: discord.PermissionOverwrite(read_messages=True, manage_messages=True),  # manage needed for pins
+            host_role: discord.PermissionOverwrite(read_messages=True, manage_messages=True),
+            spec_role: discord.PermissionOverwrite(read_messages=True),
+        }
+        topic = f"{player}'s Role PM"
+        await category.create_text_channel(player_channel_name(player), overwrites=overwrites, topic=topic)
+
+    server = session.query(hbs.Server).filter_by(id=ctx.guild.id).one_or_none()
+    # server.rolepms = hbs.RolePMs(id=category.id, server_id=ctx.guild.id)
+    server.rolepms_id = category.id
+    session.commit()
+    # TODO: save category
+
+    if len(error_names) > 0:
+        await ctx.send(error)
+
+
+@init.command(name='reset')
+@commands.is_owner()
 async def init_reset(ctx: commands.Context):
     session = session_maker()
     server = session.query(hbs.Server).filter_by(id=ctx.guild.id).one_or_none()
@@ -110,6 +191,15 @@ async def init_reset(ctx: commands.Context):
             await discord_role.delete(reason=reason)
     except discord.Forbidden:
         await ctx.send('Insufficient permissions to delete roles.')
+
+    if server.rolepms_id is not None:
+        try:
+            category = ctx.guild.get_channel(server.rolepms_id)  # type: discord.CategoryChannel
+            for channel in category.channels:
+                await channel.delete(reason=reason)
+            await category.delete(reason=reason)
+        except discord.Forbidden:
+            await ctx.send('Insufficient permissions to delete Role PMs.')
 
     session.delete(server)
     session.commit()
--- plugins/hostbot_schema.py
+++ plugins/hostbot_schema.py
@@ -3,17 +3,18 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-
 Base = declarative_base()
 
 
 class Server(Base):
     __tablename__ = 'Server'
-    id       = Column(Integer, primary_key=True)
-    name     = Column(String)
-    sheet    = Column(String)
-    roles    = relationship('Role', back_populates='server')
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    sheet = Column(String)
+    rolepms_id = Column(Integer)
+    roles = relationship('Role', back_populates='server')
     channels = relationship('Channel', back_populates='server')
+    # rolepms = relationship('RolePMs', back_populates='server', uselist=False)
 
     def __repr__(self):
         return f'<Server id={self.id}, name={self.name}, sheet={self.sheet}>'
@@ -21,10 +22,10 @@ class Server(Base):
 
 class Role(Base):
     __tablename__ = 'Role'
-    id        = Column(Integer, primary_key=True)
-    type      = Column(String)
+    id = Column(Integer, primary_key=True)
+    type = Column(String)
     server_id = Column(Integer, ForeignKey('Server.id'))
-    server    = relationship('Server', back_populates='roles')
+    server = relationship('Server', back_populates='roles')
 
     def __repr__(self):
         return (
@@ -36,10 +37,10 @@ class Role(Base):
 
 class Channel(Base):
     __tablename__ = 'Channel'
-    id        = Column(Integer, primary_key=True)
-    type      = Column(String)
+    id = Column(Integer, primary_key=True)
+    type = Column(String)
     server_id = Column(Integer, ForeignKey('Server.id'))
-    server    = relationship('Server', back_populates='channels')
+    server = relationship('Server', back_populates='channels')
 
     def __repr__(self):
         return (
@@ -47,3 +48,18 @@ class Channel(Base):
             f'type={self.type}, '
             f'server_id={self.server_id}>'
         )
+
+
+# class RolePMs(Base):
+#     __tablename__ = 'RolePMs'
+#     # id = Column(Integer, primary_key=True)
+#     # server_id = Column(Integer, ForeignKey('Server.id'))
+#     id = Column(Integer)
+#     server_id = Column(Integer, ForeignKey('Server.id'), primary_key=True)
+#     server = relationship('Server', back_populates='rolepms')
+#
+#     def __repr__(self):
+#         return (
+#             f'<Channel id={self.id}, '
+#             f'server_id={self.server_id}>'
+#         )
